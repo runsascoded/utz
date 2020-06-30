83125ca5ad37b64d85104675fbc5b48b773f05e5
Ryan Williams <ryan.williams@mssm.edu>
2020-06-30 14:10:34 +0000

    git, process, papermill improvements

--- _git/__init__.py
+++ _git/__init__.py
@@ -4,3 +4,28 @@ try:
     Repo = partial(Repo, search_parent_directories=True)
 except ImportError:
     pass
+
+
+from .remote import push
+from .process import run
+
+
+def checkout(branch, ref=None, exist=False):
+    if exist:
+        run('git','checkout',branch,ref)
+    else:
+        run('git','checkout','-B',branch,ref)
+
+
+def merge(ref, msg=None):
+    cmd = ['git','merge','--no-edit']
+    if msg: cmd += ['-m',msg]
+    run(cmd)
+
+
+def commit(msg=None, all=False, amend=False):
+    cmd = ['git','commit']
+    if amend: cmd += ['--amend']
+    if all: cmd += ['-a']
+    if msg: cmd += ['-m',msg]
+    run(cmd)
