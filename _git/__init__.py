32935ef5332821d13062f63bec25ce28083bb7f5
Ryan Williams <ryan@runsascoded.com>
2020-07-12 17:51:11 +0000

    fix Repo patch

--- _git/__init__.py
+++ _git/__init__.py
@@ -1,7 +1,7 @@
 try:
     from git import Repo, Git
     from functools import partial
-    Repo = partial(Repo, search_parent_directories=True)
+    Repo.__call__ = partial(Repo, search_parent_directories=True)
 except ImportError:
     pass
 
