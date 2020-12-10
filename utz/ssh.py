295c8bacb09348afd57ccb2dcead3ae8e52ba69c
Ryan Williams <ryan@runsascoded.com>
2020-12-10 11:16:08 -0500

    use Popen instead of Thread for ssh tunnel

--- utz/ssh.py
+++ utz/ssh.py
@@ -1,43 +1,9 @@
-from contextlib import AbstractContextManager, contextmanager
-from ctypes import py_object, pythonapi
 import subprocess
-from subprocess import check_call, CalledProcessError, PIPE
-import threading
-from threading import Thread
 import time
+from contextlib import AbstractContextManager
+from subprocess import CalledProcessError, PIPE, Popen
 
-from .process import check, run
-
-
-class CtxThread(AbstractContextManager, Thread):
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None):
-        Thread.__init__(self, group=group, target=target, name=name, args=args, kwargs=kwargs)
-        AbstractContextManager.__init__(self)
-
-    def __enter__(self):
-        print('CtxThread.__enter__')
-        self.start()
-        print('CtxThread started thread')
-
-    def get_id(self):
-        # returns id of the respective thread 
-        if hasattr(self, '_thread_id'):
-            return self._thread_id
-        for id, thread in threading._active.items():
-            if thread is self:
-                return id
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print(f'CtxThread.__exit__({exc_type}, {exc_val}, {exc_tb})')
-        thread_id = self.get_id()
-        print(f'thread_id: {thread_id}')
-        res = pythonapi.PyThreadState_SetAsyncExc(thread_id, py_object(SystemExit))
-        if res > 1:
-            pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
-            print('Exception raise failure')
-
-    def stop(self):
-        self.__exit__(None, None, None)
+from utz.process import check
 
 
 class Tunnel(AbstractContextManager):
@@ -77,14 +43,9 @@ class Tunnel(AbstractContextManager):
     def start(self):
         return self.__enter__()
 
-    @staticmethod
-    def ssh_tunnel(src, dst, proxy):
-        run('ssh','-N','-L',f'{src}:{dst}',proxy)
-
     def __enter__(self):
         print('Tunnel.__enter__')
-        self.ctx_thread = CtxThread(target=self.ssh_tunnel, args=(self.src, self.dst, self.proxy))
-        self.ctx_thread.__enter__()
+        self.proc = Popen(['ssh','-N','-L',f'{self.src}:{self.dst}',self.proxy])
         try:
             print('entered ctxthread')
             if self.sleep is not None:
@@ -101,12 +62,12 @@ class Tunnel(AbstractContextManager):
                     raise CalledProcessError(p.returncode, cmd)
         except Exception as e:
             print('Caught exception verifying tunnel')
-            self.ctx_thread.__exit__(type(e), e, e.__traceback__)
+            self.proc.kill()
             raise e
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print(f'Tunnel.__exit__({exc_type}, {exc_val}, {exc_tb})')
-        self.ctx_thread.__exit__(exc_type, exc_val, exc_tb)
+        self.proc.kill()
 
     def stop(self): self.__exit__(None, None, None)
     def close(self): self.__exit__(None, None, None)
