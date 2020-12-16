c7df52c18a09923d8733de42e7eef3f7fe7f42f2
Ryan Williams <ryan@runsascoded.com>
2020-12-16 11:51:59 -0500

    git helper improvements

--- utz/git/clone.py
+++ utz/git/clone.py
@@ -3,23 +3,31 @@ from contextlib import contextmanager
 from tempfile import TemporaryDirectory
 from typing import Iterable
 
-from utz import cd, run
+import utz
+from utz import nullcontext, run
 
 
 @contextmanager
-def tmp(url, *clone_args, submodules=True, push=False, **run_kwargs):
+def tmp(url, *clone_args, submodules=True, push=False, cd=True, **run_kwargs):
     with TemporaryDirectory() as tmpdir:
         cmd = ['git','clone']
         if submodules: cmd += ['--recurse-submodules']
         cmd += clone_args
         cmd += [ url,tmpdir, ]
         run(*cmd, **run_kwargs)
-        with cd(tmpdir):
+        if cd:
+            ctx = utz.cd(tmpdir)
+            post_ctx = nullcontext()
+        else:
+            ctx = nullcontext()
+            post_ctx = utz.cd(tmpdir)
+        with ctx:
             yield tmpdir
-            if push:
-                cmd = ['git','push']
-                if isinstance(push, str):
-                    cmd += ['origin',push]
-                elif isinstance(push, Iterable):
-                    cmd += push
-                run(*cmd)
+            with post_ctx:
+                if push:
+                    cmd = ['git','push']
+                    if isinstance(push, str):
+                        cmd += ['origin',push]
+                    elif isinstance(push, Iterable):
+                        cmd += push
+                    run(*cmd)
--- utz/git/head.py
+++ utz/git/head.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from ..process import line
-def sha(): return line('git','log','-n1','--format=%h')
-def subject(): return line('git','log','-n1','--format=%s')
 
+
+def sha(*args,**kwargs):
+    return line('git','log','-n1','--format=%h',*args,**kwargs)
+
+
+def subject(*args,**kwargs):
+    return line('git','log','-n1','--format=%s',*args,**kwargs)
