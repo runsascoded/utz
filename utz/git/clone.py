5ab2fb7b388824e88f6e02030265653bb585be6d
Ryan Williams <ryan@runsascoded.com>
2020-12-16 10:48:31 -0500

    optionally push tmp clone results

--- utz/git/clone.py
+++ utz/git/clone.py
@@ -1,12 +1,13 @@
 
 from contextlib import contextmanager
 from tempfile import TemporaryDirectory
+from typing import Iterable
 
 from utz import cd, run
 
 
 @contextmanager
-def tmp(url, *clone_args, submodules=True, **run_kwargs):
+def tmp(url, *clone_args, submodules=True, push=False, **run_kwargs):
     with TemporaryDirectory() as tmpdir:
         cmd = ['git','clone']
         if submodules: cmd += ['--recurse-submodules']
@@ -15,3 +16,10 @@ def tmp(url, *clone_args, submodules=True, **run_kwargs):
         run(*cmd, **run_kwargs)
         with cd(tmpdir):
             yield tmpdir
+            if push:
+                cmd = ['git','push']
+                if isinstance(push, str):
+                    cmd += ['origin',push]
+                elif isinstance(push, Iterable):
+                    cmd += push
+                run(*cmd)
