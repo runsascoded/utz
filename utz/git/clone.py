1366588ce03ca0aafc5fa5221127e5dfc2660ef9
Ryan Williams <ryan@runsascoded.com>
2020-12-21 11:27:40 -0500

    git tmp clone utility + test

--- utz/git/clone.py
+++ utz/git/clone.py
@@ -1,33 +1,119 @@
 
 from contextlib import contextmanager
-from tempfile import TemporaryDirectory
+from os.path import basename
+from sys import stderr
 from typing import Iterable
 
 import utz
-from utz import nullcontext, run
+from utz import check, git, line, match, now, print_exc, run, tmpdir, uuid1
 
 
 @contextmanager
-def tmp(url, *clone_args, submodules=True, push=False, cd=True, **run_kwargs):
-    with TemporaryDirectory() as tmpdir:
+def tmp(
+    url,
+    *clone_args,
+    branch=None,
+    checkout=None,
+    init=None,
+    submodules=True,
+    push=False,
+    pull=False,
+    cd=True,
+    name=None,
+    **run_kwargs,
+):
+    '''contextmanager for creating a Git repo in a temporary directory, and optionally cd'ing into it and upstreaming
+    commits from it
+
+    - `url`: local or remote path to Git repository to be cloned
+    - `clone_args` (List[str]): passed directly to `git clone`
+    - `branch`: Git branch to operate on inside temporary clone (by default, inherit from origin)
+    - `checkout`: ref to checkout `branch` to
+    - `init`: like `checkout`, but only used if `branch` doesn't already exist
+    - `submodules`: recursively clone submodules (`git clone --recurse-submodules`)
+    - `push` (str | bool | List[str]): run a `git push` after `yield`ing; `str` or `List[str]` serve as arguments to
+        `git push`
+    - `pull` (bool): upstream changes post-`yield` by `cd`ing into the origin directory (must be a local dir, otherwise
+        use `push`) and running a `git pull` from this temporary clone
+    - `cd` (bool): move into the temporary clone dir before `yield`ing
+    - `name`: basename for the temporary clone directory (defaults to basename of `url`)
+    '''
+    if checkout and init:
+        raise ValueError(f'checkout and init are exclusive')
+
+    name = name or basename(url)
+    if name.endswith('.git'): name = name[:-len('.git')]
+    with tmpdir(name) as repo_dir:
         cmd = ['git','clone']
         if submodules: cmd += ['--recurse-submodules']
+        if branch and not checkout and not init:
+            # branch already exists and we want to clone and work on it
+            cmd += ['-b',branch]
         cmd += clone_args
-        cmd += [ url,tmpdir, ]
+        cmd += [ url, repo_dir, ]
         run(*cmd, **run_kwargs)
+        # `checkout` and `init` are similar in that they provide a starting ref for the branch, but `init` no-ops if the
+        # branch already exists (it is only used when a new working branch is being created)
+        if checkout or init:
+            ref = checkout or init
+            with utz.cd(repo_dir):
+                if ref is True:
+                    ref = git.branch.current()
+
+                update_branch = True
+                if branch:
+                    if git.branch.exists(branch) and init:
+                        update_branch = False
+                else:
+                    branch = f'tmp-{now("short")}'
+
+                if update_branch:
+                    run('git','checkout','-b',branch,ref)
+
+                if cd:
+                    yield repo_dir
         if cd:
-            ctx = utz.cd(tmpdir)
-            post_ctx = nullcontext()
+            if not (checkout or init):
+                # if `checkout or init`, we've already cd'd and yielded above
+                with utz.cd(repo_dir):
+                    yield repo_dir
         else:
-            ctx = nullcontext()
-            post_ctx = utz.cd(tmpdir)
-        with ctx:
-            yield tmpdir
-            with post_ctx:
-                if push:
-                    cmd = ['git','push']
-                    if isinstance(push, str):
-                        cmd += ['origin',push]
-                    elif isinstance(push, Iterable):
-                        cmd += push
-                    run(*cmd)
+            yield repo_dir
+
+        # optionally cd into upstream `url` dir (assumed to be a local directory if `pull` is set) and pull changes
+        # from the temporary `repo_dir`
+        if pull:
+            with utz.cd(url):
+                if not branch: branch = git.branch.current()
+                run('git','fetch',repo_dir)
+                try:
+                    ref_line = line('git','ls-remote',repo_dir,f'refs/heads/{branch}')
+                    m = match(
+                        '(?P<sha>[0-9a-f]{40})\s+refs/heads/%s' % branch,
+                        ref_line,
+                    )
+                    if m:
+                        sha = m['sha']
+                    else:
+                        raise RuntimeError(f'Unrecognized ls-remote line: {ref_line}')
+                except Exception as e:
+                    stderr.write('%s\n' % f'Error fetching branch {branch} from remote {repo_dir}')
+                    print_exc()
+                    sha = git.sha('FETCH_HEAD')
+
+                if not check('git','merge','-m','\n'.join([f'Merge {branch} from tmpdir','',repo_dir]),sha):
+                    short_sha = git.fmt('%h', sha)
+                    tmp_branch = f'conflict-{short_sha}'
+                    run('git','branch',tmp_branch,sha)
+                    stderr.write('Pulling from %s failed, aborting merge, saving to %s\n' % (repo_dir, tmp_branch))
+                    run('git','merge','--abort')
+
+        # optionally push back upstream
+        if push:
+            with utz.cd(repo_dir):
+                cmd = ['git','push']
+                if isinstance(push, str):
+                    cmd += ['origin',push]
+                elif isinstance(push, Iterable):
+                    cmd += push
+                run(*cmd)
--- utz/tests/test_git.py
+++ utz/tests/test_git.py
@@ -1,13 +1,165 @@
 
-from utz import exists, getcwd, git, lines, realpath
+import utz
+from utz import basename, cd, dirname, exists, getcwd, git, join, lines, realpath, run
 
 
-def test_tmp_clone():
-    tmpdir = None
+def test_tmp_clone_remote():
     with git.clone.tmp('https://gitlab.com/gsmo/examples/factors.git') as cwd:
         # Check that we're actually in the directory returned by the ctx mgr above:
         tmpdir = realpath(getcwd())
         assert tmpdir == realpath(cwd)
+        assert basename(getcwd()) == 'factors'
         # It's a clean clone
         assert not lines('git','status','--short')
+
+    # verify the tmpdir is gone
     assert not exists(tmpdir)
+
+def test_tmp_clone_local():
+    branch = 'tmp'
+    tag = 'v0.0.1'
+    base_repo = join(dirname(utz.__file__), 'tests/data/gsmo')
+    sha0 = '68a257a'
+
+    def check(cwd=None, name=None, status=True, branch=None, paths=None, shas=None, files=None, rm=None,):
+        if cwd:
+            # Check that we're actually in the directory returned by the ctx mgr above:
+            dir = realpath(getcwd())
+            assert dir == realpath(cwd)
+
+        if name:
+            # check basename
+            assert basename(getcwd()) == name
+
+        if status:
+            if status is True:
+                # verify clone is clean
+                assert not lines('git','status','--short')
+            else:
+                # verify status lines
+                assert lines('git','status','--short') == status
+
+        # verify current branch
+        if branch:
+            assert git.branch.current() == branch
+
+        # verify submodules have been cloned
+        if paths:
+            if isinstance(paths, str): paths = [paths]
+            for path in paths:
+                assert exists(path), f"{path} doesn't exist"
+
+        # verify various shas
+        for sha, refs in shas.items():
+            if isinstance(refs, (list,tuple)):
+                for ref in refs:
+                    if ref is None:
+                        assert git.sha() == sha
+                    else:
+                        assert git.sha(ref) == sha
+
+        if files:
+            for path, lns in files.items():
+                assert lines('cat',path) == lns
+
+        if rm:
+            assert not exists(rm)
+
+    # bind some `check` params
+    def verify(wd, sha):
+        check(
+            cwd=wd,
+            name='gsmo',
+            paths='example/hailstone/run.ipynb',
+            branch=branch,
+            shas={sha: (None, branch)},
+        )
+
+    def commit_file(path, lines, parent_sha, mode='w'):
+        new_file = not exists(path)
+        with open(path, mode) as f:
+            if isinstance(lines, str): lines = [lines]
+            f.writelines('%s\n' % ln for ln in lines)
+        run('git','add',path)
+        if new_file:
+            msg = f'add {path}: {lines}'
+        else:
+            msg = f'update {path}: {lines}'
+        run('git','commit','-m',msg)
+        assert git.sha('HEAD^') == parent_sha
+        return git.sha()
+
+    with git.clone.tmp(base_repo, branch=branch, init=tag) as origin:
+        verify(origin, sha0)
+
+        # simulate an additional clone + pulling changes back in
+        with git.clone.tmp(origin, branch=branch, pull=True) as wd:
+            tmpdir = wd
+            verify(wd, sha0)
+            sha1 = commit_file('test1.txt', ['123','456'], sha0)
+
+        check(
+            shas={ sha1: (None, branch) },
+            branch=branch,
+            files={ 'test1.txt': ['123','456'] },
+            rm=tmpdir,  # verify the tmpdir is gone
+        )
+
+        # simulate an additional clone + merging changes back in with concurrent upstream changes
+        with git.clone.tmp(origin, branch=branch, pull=True) as wd:
+            tmpdir = wd
+            verify(wd, sha1)
+            sha2_2 = commit_file('test2.txt', ['abc','def'], sha1)
+
+            # meanwhile, make another change in origin (which will have to be merged w/ sha2)
+            with cd(origin):
+                sha2_1 = commit_file('test1.txt', '789', sha1, mode='a')
+
+            # on ctx mgr exit, merge is performed…
+
+        sha2 = git.sha()
+        check(
+            shas={
+                sha2: branch,
+                sha2_1: 'HEAD^',
+                sha2_2: 'HEAD^2',
+            },
+            branch=branch,
+            files={
+                'test1.txt': ['123','456','789',],
+                'test2.txt': ['abc','def',],
+            },
+            rm=tmpdir,
+        )
+
+        # simulate an additional clone + failing to merge changes in vs concurrent upstream changes, but leaving a ref
+        # for safe-keeping
+        with git.clone.tmp(origin, branch=branch, pull=True) as wd:
+            tmpdir = wd
+            verify(wd, sha2)
+            sha3_2 = commit_file('test2.txt', 'zzz', sha2, mode='a')
+
+            # meanwhile, make another change in origin (which will have to be merged w/ sha2)
+            with cd(origin):
+                sha3_1 = commit_file('test2.txt', 'yyy', sha2, mode='a')
+
+            # on ctx mgr exit, merge is performed, but a merge conflict arises, and a new branch `conflict-<sha>` points
+            # at the unmerged temporary/remote changes
+
+        conflict_branch = f'conflict-{git.fmt("%h", sha3_2)}'
+        check(
+            shas={
+                sha3_1: (None, branch),
+                sha3_2: conflict_branch,
+                sha2: 'HEAD^',
+            },
+            branch=branch,
+            files={
+                'test1.txt': ['123','456','789',],
+                'test2.txt': ['abc','def','yyy',],
+            },
+            rm=tmpdir,
+        )
+
+    # verify the tmpdir is gone
+    assert not exists(origin)
