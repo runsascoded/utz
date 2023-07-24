d82fde000f51860b0db3b8ebdebb5f9f623d326c
Ryan Williams <ryan@runsascoded.com>
2023-07-23 20:21:38 -0400

    v0.3.27

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from utz.setup import setup
 
 setup(
     name="utz",
-    version="0.3.26",
+    version="0.3.27",
     install_requires=open('requirements.txt', 'r').read(),
     extras_require={
         'pdf': [
