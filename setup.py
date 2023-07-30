becbb8ad98ba3157605dd3e864a78fc210713890
Ryan Williams <ryan@runsascoded.com>
2023-07-29 21:51:23 -0400

    bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from utz.setup import setup
 
 setup(
     name="utz",
-    version="0.3.28",
+    version="0.4.0",
     install_requires=open('requirements.txt', 'r').read(),
     extras_require={
         'pdf': [
