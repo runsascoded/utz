980ac640213b88305608e8d2060a418538fed4ee
Ryan Williams <ryan@runsascoded.com>
2023-08-02 19:25:27 -0400

    add github.repository_option

--- utz/git/github.py
+++ utz/git/github.py
@@ -1,6 +1,10 @@
+import click
+from functools import wraps
+from os import environ
 import re
 from sys import stderr
 from typing import Optional, Literal
+from utz import process
 
 GITHUB_HTTPS_URL_RGX = r'https://github.com/(?P<nameWithOwner>[^/]+/[^/]+?)(?:\.git)?'
 GITHUB_SSH_URL_RGX = r'git@github.com:(?P<nameWithOwner>[^/]+/[^/]+?)(?:\.git)?'
@@ -18,3 +22,30 @@ def parse_url(url: str, err: Literal['raise', 'stderr', 'none'] = 'raise') -> Op
     elif err == 'stderr':
         print(f'Could not parse GitHub url: {url}', file=stderr)
     return None
+
+
+GITHUB_REPOSITORY = 'GITHUB_REPOSITORY'
+
+
+def repository_option(
+        *flag_args,
+        env=GITHUB_REPOSITORY,
+        help='Repository name with owner, e.g. "owner/repo"', **flag_kwargs,
+):
+    if not flag_args:
+        flag_args = ('-R', '--repository')
+
+    def option(fn):
+        @click.option(*flag_args, help=help, **flag_kwargs)
+        @wraps(fn)
+        def _fn(*args, repository=None, **kwargs):
+            if not repository:
+                repository = environ.get(env)
+                if not repository:
+                    verbose = kwargs.get('verbose', 0)
+                    repository = process.json('gh', 'repo', 'view', '--json', 'nameWithOwner', log=None if verbose else False)['nameWithOwner']
+            return fn(*args, repository=repository, **kwargs)
+
+        return _fn
+
+    return option
