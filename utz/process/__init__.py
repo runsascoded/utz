43256acdf8e45ea2e242994e6e2ec49332f7ec4d
Ryan Williams <ryan.williams@tiledb.com>
2024-08-09 13:35:58 -0400

    add `both` kwarg to `process.{output,lines}`
    also type annotations to `process`

--- utz/process/__init__.py
+++ utz/process/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+import shlex
 from functools import partial
 from json import loads
-import shlex
-from subprocess import check_call, check_output, CalledProcessError, DEVNULL
+from subprocess import check_call, check_output, CalledProcessError, DEVNULL, Popen, PIPE, STDOUT
 from sys import stderr
-
+from typing import Optional, List, Tuple, Callable, Union
 
 err = partial(print, file=stderr)
 
@@ -13,7 +13,7 @@ def silent(*args):
     pass
 
 
-def parse_cmd(cmd):
+def parse_cmd(cmd: Tuple[str, ...]) -> List[str]:
     """Stringify and potentially unwrap a command"""
 
     def flatten(args):
@@ -33,7 +33,16 @@ def parse_cmd(cmd):
     ]
 
 
-def lines(*cmd, keep_trailing_newline=False, dry_run=False, err_ok=False, **kwargs):
+Arg = Union[None, str, list['Arg']]
+
+
+def lines(
+        *cmd: Arg,
+        keep_trailing_newline: bool = False,
+        dry_run: bool = False,
+        err_ok: bool = False,
+        **kwargs,
+) -> Optional[List[str]]:
     """Return the lines written to stdout by a command"""
     out = output(*cmd, dry_run=dry_run, err_ok=err_ok, **kwargs)
     if err_ok and out is None:
@@ -51,7 +60,7 @@ def lines(*cmd, keep_trailing_newline=False, dry_run=False, err_ok=False, **kwar
     return lines
 
 
-def line(*cmd, empty_ok=False, err_ok=False, **kwargs):
+def line(*cmd: str, empty_ok: bool = False, err_ok: bool = False, **kwargs) -> Optional[str]:
     """Run a command, verify that it returns a single line of output, and return that line"""
     _lines = lines(*cmd, err_ok=err_ok, **kwargs)
     if (empty_ok or err_ok) and not _lines:
@@ -63,19 +72,27 @@ def line(*cmd, empty_ok=False, err_ok=False, **kwargs):
 
 
 ELIDED = '****'
+Elides = Optional[List[str]]
+Log = Optional[Callable[..., None]]
 
 
-def mk_cmd_str(cmd, elide=None):
-    shlex_join = getattr(shlex, 'join', ' '.join)  # shlex.join only exists in Python ≥3.8
-    cmd_str = shlex_join(cmd)
+def mk_cmd_str(cmd: List[str], elide: Elides = None) -> str:
+    cmd_str = shlex.join(cmd)
     if elide:
-        if isinstance(elide, str): elide = [elide]
+        if isinstance(elide, str):
+            elide = [elide]
         for s in elide:
             cmd_str = cmd_str.replace(s, ELIDED)
     return cmd_str
 
 
-def run(*cmd, dry_run=False, elide=None, log=err, **kwargs):
+def run(
+        *cmd: str,
+        dry_run: bool = False,
+        elide: Elides = None,
+        log: Log = err,
+        **kwargs,
+) -> None:
     """Convenience wrapper for check_call"""
     cmd = parse_cmd(cmd)
     cmd_str = mk_cmd_str(cmd, elide)
@@ -91,8 +108,45 @@ def run(*cmd, dry_run=False, elide=None, log=err, **kwargs):
 sh = run
 
 
-def output(*cmd, dry_run=False, err_ok=False, elide=None, log=err, **kwargs):
-    """Convenience wrapper for check_output"""
+def interleaved_output(cmd: List[str], err_ok: bool = False) -> bytes:
+    try:
+        proc = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+
+        # Stream and capture the output in real-time
+        output = b''
+        for line in proc.stdout:
+            output += line
+
+        proc.wait()
+        if proc.returncode != 0:
+            raise CalledProcessError(proc.returncode, cmd, output=output)
+        else:
+            return output
+    except CalledProcessError as e:
+        if err_ok:
+            return e.output
+        else:
+            err(e.output.decode())
+            raise e
+
+
+def output(
+        *cmd: Arg,
+        dry_run: bool = False,
+        both: bool = False,
+        err_ok: bool = False,
+        elide: Elides = None,
+        log: Log = err,
+        **kwargs,
+) -> Optional[bytes]:
+    """Convenience wrapper for `check_output`
+
+    By default, logs commands to `err` (stderr) before running (pass `log=None` to disable).
+
+    If `err_ok=True`, exceptions will be caught, and `None` returned.
+
+    If `both=True`, stdout and stderr will both be captured (interleaved), and returned.
+    """
     cmd = parse_cmd(cmd)
     cmd_str = mk_cmd_str(cmd, elide)
     if dry_run:
@@ -102,16 +156,19 @@ def output(*cmd, dry_run=False, err_ok=False, elide=None, log=err, **kwargs):
     else:
         if log:
             log(f'Running: {cmd_str}')
-        try:
-            return check_output(cmd, **kwargs)
-        except CalledProcessError as e:
-            if err_ok:
-                return None
-            else:
-                raise e
+        if both:
+            return interleaved_output(cmd, err_ok=err_ok)
+        else:
+            try:
+                return check_output(cmd, **kwargs)
+            except CalledProcessError as e:
+                if err_ok:
+                    return None
+                else:
+                    raise e
 
 
-def json(*cmd, dry_run=False, **kwargs):
+def json(*cmd: str, dry_run: bool = False, **kwargs) -> Optional[dict]:
     """Run a command, parse the output as JSON, and return the parsed object"""
     out = output(*cmd, dry_run=dry_run, **kwargs)
     if out is None:
@@ -119,7 +176,7 @@ def json(*cmd, dry_run=False, **kwargs):
     return loads(out.decode())
 
 
-def check(*cmd, stdout=DEVNULL, stderr=DEVNULL, **kwargs):
+def check(*cmd: str, stdout=DEVNULL, stderr=DEVNULL, **kwargs) -> bool:
     """Return True iff a command run successfully (i.e. exits with code 0)"""
     try:
         run(*cmd, stdout=stdout, stderr=stderr, **kwargs)
@@ -129,4 +186,4 @@ def check(*cmd, stdout=DEVNULL, stderr=DEVNULL, **kwargs):
 
 
 # Omit "json", to avoid colliding with stdlib
-__all__ = ['check', 'err', 'line', 'lines', 'output', 'run', 'sh', 'silent', ]
+__all__ = [ 'check', 'err', 'interleaved_output', 'line', 'lines', 'output', 'run', 'sh', 'silent', ]
