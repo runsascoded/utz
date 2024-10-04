1aa85d4673b98dfa3cb69cba6872fa131b67afd9
Ryan Williams <ryan@runsascoded.com>
2024-10-03 22:44:29 -0400

    `mkdir`/`mkpar`: types, `str | Path`

--- utz/path.py
+++ utz/path.py
@@ -3,13 +3,16 @@ from os.path import dirname
 from pathlib import Path
 
 
-def mkdir(path, *args, exist_ok=True, **kwargs):
-    makedirs(str(path), *args, exist_ok=exist_ok, **kwargs)
+def mkdir(path: str | Path, mode=0o777, exist_ok=True) -> str | Path:
+    """Wrapper for ``makedirs``, accepting ``str | Path``, and setting ``exist_ok=True`` by default."""
+    makedirs(str(path), mode=mode, exist_ok=exist_ok)
     return path
 
-def mkpar(path, *args, **kwargs):
+
+def mkpar(path: str | Path, *args, **kwargs) -> str | Path:
+    """Create parent directories for a path, if they do not exist."""
     if isinstance(path, Path):
-        path.parent.mkdir(exist_ok=True, parents=True)
+        mkdir(path.parent, *args, **kwargs)
     else:
         mkdir(dirname(path))
     return path
