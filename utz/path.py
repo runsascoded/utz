f4e92bcdd6413f090e71f66f936b7ce8be10d212
Ryan Williams <ryan.williams@tiledb.com>
2024-10-11 10:02:54 -0400

    `path.py` fix

--- utz/path.py
+++ utz/path.py
@@ -14,7 +14,7 @@ def mkdir(path: Path, mode=0o777, exist_ok=True) -> Path:
 
 def mkpar(path: Path, *args, **kwargs) -> Path:
     """Create parent directories for a path, if they do not exist."""
-    if isinstance(path, Path):
+    if isinstance(path, pathlib.Path):
         mkdir(path.parent, *args, **kwargs)
     else:
         mkdir(dirname(path))
