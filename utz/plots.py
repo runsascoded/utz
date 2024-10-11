278cd923673c10b67db85d499f0d18342b80e979
Ryan Williams <ryan.williams@tiledb.com>
2024-10-11 10:03:04 -0400

    `utz.plots.symbols` comment/nit

--- utz/plots.py
+++ utz/plots.py
@@ -12,9 +12,11 @@ import plotly.graph_objects as go
 from IPython.display import Image
 from plotly.graph_objs import Figure
 from plotly.validators.scatter.marker import SymbolValidator
-symbols = SymbolValidator().values[2::12]
 
 from utz import err
+
+# Plotly marker names
+symbols = SymbolValidator().values[2::12]
 
 
 # Env vars to fall back to, for various plot kwargs
