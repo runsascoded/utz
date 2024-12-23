cfc0e19cdf8ebf47c9a7a83aa08e610e861629e7
Ryan Williams <ryan@runsascoded.com>
2024-12-23 09:34:28 -0500

    v0.12.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras_require['all'] = sum(extras_require.values(), [ 'pyyaml', ])
 
 setup(
     name="utz",
-    version="0.11.5",
+    version="0.12.0",
     install_requires=["stdlb"],
     extras_require=extras_require,
     url="https://github.com/runsascoded/utz",
