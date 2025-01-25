7f4ed343e89d808048e0032945c02b4033ff6815
Ryan Williams <ryan@runsascoded.com>
2025-01-25 17:01:58 -0500

    v0.14.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extras_require['all'] = sum(extras_require.values(), [ 'pyyaml', ])
 
 setup(
     name="utz",
-    version="0.14.0",
+    version="0.14.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=["stdlb"],
