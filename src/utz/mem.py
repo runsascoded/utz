34a772b2516480cab15a03f5b97fc8e77caecf6f
Ryan Williams <ryan@runsascoded.com>
2025-02-25 17:06:35 -0500

    `mem.Tracker`: `follow_forks=True`, mkdir, log

--- src/utz/mem.py
+++ src/utz/mem.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from typing import Literal
+
 import json
-from os import getcwd, remove
-from os.path import splitext, exists
+from os import getcwd, remove, makedirs
+from os.path import splitext, exists, dirname
 from subprocess import DEVNULL
 from tempfile import NamedTemporaryFile
 from types import TracebackType
@@ -19,18 +21,28 @@ class Tracker:
         path: str | None = None,
         keep: bool | None = None,
         native_traces: bool = True,
-        log: Log | bool = None,
+        follow_fork: bool = True,
+        log: Log | bool | Literal[0, 1, 2] = None,
         **kwargs,
     ):
         self.path = path
         self.keep = keep
+        self.verbose = 1
         if log is True:
+            log = err
+        elif isinstance(log, int) and log > 0:
+            self.verbose = log
             log = err
         elif not log:
             log = silent
+            self.verbose = 0
         self.log = log
         self.tmpfile = False
-        self.kwargs = dict(native_traces=native_traces, **kwargs)
+        self.kwargs = dict(
+            native_traces=native_traces,
+            follow_fork=follow_fork,
+            **kwargs,
+        )
         self.peak_mem = None
         self.stats = None
         self.tracker = None
@@ -46,6 +58,7 @@ class Tracker:
             self.log(f"mem.Tracker removing {path}")
             remove(path)
 
+        makedirs(dirname(path), exist_ok=True)
         self.log(f"memray logging to {path}")
         self.tracker = memray.Tracker(path, **self.kwargs)
         self.tracker.__enter__()
@@ -70,7 +83,13 @@ class Tracker:
         rm = self.keep is False or (self.keep is None and self.tmpfile)
         stats_path = self.stats_path
         if not exc_value:
-            proc.run('memray', 'stats', '--json', '-fo', stats_path, self.path, log=self.log, stdout=DEVNULL)
+            proc.run(
+                'memray', 'stats', '--json',
+                '-fo', stats_path,
+                self.path,
+                log=self.log,
+                **(dict() if self.verbose > 1 else dict(stdout=DEVNULL)),
+            )
         if rm:
             remove(self.path)
             self.path = None
